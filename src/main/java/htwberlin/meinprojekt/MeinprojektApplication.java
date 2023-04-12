5e03801bfb3a3e0e1cb720b0736f2cc95fbafe28
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2023-04-12 14:07:39 +0200

    generated spring project

---
+++ src/main/java/htwberlin/meinprojekt/MeinprojektApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.meinprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeinprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeinprojektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htwberlin/meinprojekt/MeinprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package htwberlin.meinprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeinprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
